838cd3a4e289a908bbe257fec49a8d2828a2c7ee
cart158 <boothcar002@gmail.com>
2026-06-11 16:19:46 -0600

    More sorts and comments
    Added the Heap sort and the Cocktail sort. Also added comments for each method and beginning of each file.

--- project1/Algorithm_Performance/src/algorithm_performance/Algorithm_Performance.java
+++ project1/Algorithm_Performance/src/algorithm_performance/Algorithm_Performance.java
@@ -4,44 +4,76 @@
  */
 package algorithm_performance;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 import java.util.Arrays;
+import javax.swing.*;
 
 /**
- *
- * @author booth
+ * @author Carter Booth CS 2430, Section 501 Programming Project 1 - Spring 2026
+ * This is the main file that creates a JFrame and it's components, also causes
+ * the sorts to run.
  */
 public class Algorithm_Performance
 {
 
-    /**
-     * @param args the command line arguments
-     */
-    
+    // This is all the variables used for the program.
     private static int[] nums;
     private static int size = 4;
-    private static Sorts s;
-    
+    final private static Sorts s = new Sorts();
+    final private static JFrame frame = new JFrame("Algorithm Performance");
+    final private static JPanel panel = new JPanel();
+    private static JLabel l1, l2, l3, l4, l5;
+    final private static JButton button = new JButton();
+    final private static JSpinner spinner = new JSpinner(new SpinnerNumberModel(4, 4, 20, 1));
+
+    // The Main method creates the JFrame and JPanel that display the output of the sorts.
     public static void main(String[] args)
     {
-        s = new Sorts();
-        nums = generate(size);
-        System.out.println(Arrays.toString(nums));
-        s.startMerge(nums);
-        System.out.println(Arrays.toString(nums) + ", Compares: " + s.getCycles());
-        s.startQuick(nums);
-        System.out.println(Arrays.toString(nums) + ", Compares: " + s.getCycles());
+        frame.add(panel);
+        frame.setSize(800, 200);
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        panel.add(l1 = new JLabel());
+        panel.add(l2 = new JLabel());
+        panel.add(l3 = new JLabel());
+        panel.add(l4 = new JLabel());
+        panel.add(l5 = new JLabel());
+        button.setText("Refresh");
+        button.addActionListener((ActionEvent e) ->
+        {
+            size = (int) spinner.getValue();
+            refresh();
+        });
+        panel.add(spinner);
+        panel.add(button);
+        spinner.setMaximumSize(new Dimension(40, 30));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        refresh();
+        frame.setVisible(true);
     }
-    
+
+    // This generates an array of a certain size for the sorting algorithms to use.
     private static int[] generate(int size)
     {
         int[] generated;
         generated = new int[size];
-        
-        for(int i = 0; i < size; i++)
+
+        for (int i = 0; i < size; i++)
         {
             generated[i] = (int) (Math.random() * 100);
         }
-        
+
         return generated;
+    }
+
+    // This creates a new unsorted array and runs the sorts then refreshes the output.
+    private static void refresh()
+    {
+        nums = generate(size);
+        l1.setText("Unsorted Array: " + Arrays.toString(nums));
+        l2.setText("Merge Sort: " + Arrays.toString(s.startMerge(nums)) + ", Compares: " + s.getCycles());
+        l3.setText("Quick Sort: " + Arrays.toString(s.startQuick(nums)) + ", Compares: " + s.getCycles());
+        l4.setText("Heap Sort: " + Arrays.toString(s.startHeap(nums)) + ", Compares: " + s.getCycles());
+        l5.setText("Cocktail Sort: " + Arrays.toString(s.startCocktail(nums)) + ", Compares: " + s.getCycles());
     }
 }
--- project1/Algorithm_Performance/src/algorithm_performance/Sorts.java
+++ project1/Algorithm_Performance/src/algorithm_performance/Sorts.java
@@ -4,144 +4,270 @@
  */
 package algorithm_performance;
 
+import java.util.Arrays;
+
 /**
- *
- * @author booth
+ * @author Carter Booth
+ * CS 2430, Section 501
+ * Programming Project 1 - Spring 2026
+ * This file is a class that has each of the 4 sorts.
  */
 public class Sorts
 {
+    // Cycles is used to count the comparisons.
     private int cycles;
     
-    public Sorts()
-    {
-    
-    }
-    
+    // The constructor doesn't need to initialize anything.
+    public Sorts(){}
+
+    // Use this after calling a sort to get the number of comparisons.
     public int getCycles()
     {
         return cycles;
     }
     
+    // This starts the Merge sort and resets the cycles back to zero.
     public int[] startMerge(int[] unsorted)
     {
-        int[] sorted = unsorted;
+        int[] sorted = Arrays.copyOf(unsorted, unsorted.length);
         cycles = 0;
         mergeSort(sorted, 0, sorted.length - 1);
         return sorted;
     }
-    
-    public void mergeSort(int[] array, int left, int right)
+
+    // This is a recursive method that splits the array in half and calls the merge method.
+    private void mergeSort(int[] array, int left, int right)
     {
         int mid;
-        
-        if(left < right)
+
+        if (left < right)
         {
             mid = left + (right - left) / 2;
-            
+
             mergeSort(array, left, mid);
             mergeSort(array, mid + 1, right);
-            
+
             merge(array, left, mid, right);
         }
     }
-    
+
+    // This method compares the elements in the sub arrays and then merges the sub arrays into one sorted array.
     private void merge(int[] array, int left, int mid, int right)
     {
         int n1, n2;
         int[] L, R;
-        
+
         n1 = mid - left + 1;
         n2 = right - mid;
-        
+
         L = new int[n1];
         R = new int[n2];
-        
-        for(int i = 0; i < n1; i++)
+
+        for (int i = 0; i < n1; i++)
         {
             L[i] = array[left + i];
         }
-        for(int j = 0; j < n2; j++)
+        for (int j = 0; j < n2; j++)
         {
             R[j] = array[mid + 1 + j];
         }
-        
+
         int i, j, k;
-        
+
         i = 0;
         j = 0;
         k = left;
-        
-        while(i < n1 && j < n2)
+
+        while (i < n1 && j < n2)
         {
-            if(L[i] <= R[j])
+            if (L[i] <= R[j])
             {
                 array[k] = L[i];
                 i++;
-            } else {
+            } else
+            {
                 array[k] = R[j];
                 j++;
             }
             k++;
             cycles++;
         }
-        
-        while(i < n1)
+
+        while (i < n1)
         {
             array[k] = L[i];
             i++;
             k++;
         }
-        
-        while(j < n2)
+
+        while (j < n2)
         {
             array[k] = R[j];
             j++;
             k++;
         }
     }
-    
+
+    // This method starts the Quick Sort and sets cycles to zero.
     public int[] startQuick(int[] unsorted)
     {
-        int[] sorted = unsorted;
+        int[] sorted = Arrays.copyOf(unsorted, unsorted.length);
         cycles = 0;
         quickSort(sorted, 0, sorted.length - 1);
         return sorted;
     }
-    
+
+    // This method creates the pivot that the Quick sort relies on and also compares the elements in the array.
     private int quickPart(int[] array, int low, int high)
     {
         int pivot = array[high];
         int i = low - 1;
-        
-        for(int j = 0; j < high; j++)
+
+        for (int j = low; j < high; j++)
         {
             cycles++;
-            if(array[j] < pivot)
+            if (array[j] < pivot)
             {
                 i++;
                 quickSwap(array, i, j);
             }
         }
-        
+
         quickSwap(array, i + 1, high);
         return i + 1;
     }
-    
+
+    // This method is for swapping elements in the array.
     private void quickSwap(int[] array, int i, int j)
     {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
-    
+
+    // This method is recursive and uses the pivot created in the quickPart method.
     private void quickSort(int[] array, int low, int high)
     {
-        if(low < high)
+        if (low < high)
         {
             int pivot = quickPart(array, low, high);
-            
+
             quickSort(array, low, pivot - 1);
             quickSort(array, pivot + 1, high);
         }
+    }
+
+    // This method creates a binary tree that has the largest element as the root.
+    private void heapify(int[] array, int n, int i)
+    {
+        int large = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && array[left] > array[large])
+        {
+            cycles++;
+            large = left;
+        }
+
+        if (right < n && array[right] > array[large])
+        {
+            cycles++;
+            large = right;
+        }
+
+        if (large != i)
+        {
+            int temp = array[i];
+            array[i] = array[large];
+            array[large] = temp;
+
+            heapify(array, n, large);
+        }
+    }
+
+    // This method calls the heapify to create the heap then takes out one element at a time to complete the sort.
+    private void heapSort(int[] array)
+    {
+        int n = array.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+        {
+            heapify(array, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--)
+        {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            heapify(array, i, 0);
+        }
+    }
+
+    // This method starts the Heap sort and resets the cycles variable back to zero.
+    public int[] startHeap(int[] unsorted)
+    {
+        int[] sorted = Arrays.copyOf(unsorted, unsorted.length);
+        cycles = 0;
+        heapSort(sorted);
+        return sorted;
+    }
+
+    // 
+    public void cocktailSort(int[] array)
+    {
+        boolean swapped = true;
+        int start = 0;
+        int end = array.length;
+
+        while (swapped == true)
+        {
+            swapped = false;
+
+            for (int i = start; i < end - 1; i++)
+            {
+                cycles++;
+                if (array[i] > array[i + 1])
+                {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (swapped == false)
+            {
+                break;
+            }
+
+            swapped = false;
+            end--;
+
+            for (int i = end - 1; i >= start; i--)
+            {
+                cycles++;
+                if (array[i] > array[i + 1])
+                {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            start++;
+        }
+    }
+
+    // This method starts the cocktail sort and sets cycles to zero.
+    public int[] startCocktail(int[] unsorted)
+    {
+        int[] sorted = Arrays.copyOf(unsorted, unsorted.length);
+        cycles = 0;
+        cocktailSort(sorted);
+        return sorted;
     }
 }
